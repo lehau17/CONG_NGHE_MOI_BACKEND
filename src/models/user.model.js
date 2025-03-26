d1c5934030ee897fa0faa79d899359a475450c53
lehau17 <haule@hitek.com.vn>
2025-03-26 10:26:34 +0700

    FIX: fix user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -46,8 +46,42 @@ const userSchema = new mongoose.Schema({
         type: String,
         enum: ["active", "deactive"],
         default : "active"
+    },
+    background: {
+        type: String,
+        default: ""
+    },
+    is_twofa_enabled: {
+        type: Boolean,
+        default : false
+    },
+    twofa_method : {
+        type: String,
+        enum: ["OTP_EMAIL"],
+        default: "OTP_EMAIL"
+    },
+    is_visible_dob: {
+        type: String,
+        enum: ["ONLY_DAY_MONTH", "FULL", "NO_VISIBLE"],
+        default : "FULL"
+    },
+    allow_message: {
+        type: String,
+        enum: ["EVERY_ONE", "FRIEND"],
+        default : "EVERY_ONE"
+    },
+    allow_search_by_phone: {
+        type: Boolean,
+        default : true
+    },
+    enable_fast_message: {
+        type: Boolean,
+        default : true
+    },
+    list_fast_message: {
+        type: Array,
+        default : []
     }
-
 },
     { timestamps: true }
 )
