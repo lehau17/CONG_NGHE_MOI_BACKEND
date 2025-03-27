fac63e30ec5f22d592ae8eb4be0f21bb1cf6b01d
Nguyen Truong An <nguyentruongan0610@gmail.com>
2025-03-27 12:02:21 +0700

    complete get user message history

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,12 +1,12 @@
 import Conversation from "../models/conversation.model.js";
-import Message from "../models/message.model.js"; 
-import User from "../models/user.model.js"; 
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
 
 export const sendMessage = async (req, res) => {
     try {
         const { message } = req.body;
         const { id: receiverId } = req.params;
-        const senderId = req.user._id; 
+        const senderId = req.user._id;
         const receiver = await User.findById(receiverId);
         if (!receiver) {
             return res.status(404).json({ error: "Receiver not found" });
@@ -18,7 +18,7 @@ export const sendMessage = async (req, res) => {
         if (!conversation) {
             conversation = await Conversation.create({
                 members: [senderId, receiverId],
-                messages: [] 
+                messages: []
             });
         }
         const newMessage = new Message({
@@ -26,15 +26,28 @@ export const sendMessage = async (req, res) => {
             receiverId,
             message
         });
-       
-        if(newMessage) {
+
+        if (newMessage) {
             conversation.messages.push(newMessage._id);
         }
-        await Promise.all([conversation.save(),newMessage.save()]);
-        res.status(201).json(savedMessage);
+        await Promise.all([conversation.save(), newMessage.save()]);
+        res.status(201).json(newMessage);
 
     } catch (error) {
         console.log("Error in sendMessage controller:", error.message);
         res.status(500).json({ error: "Internal server error" });
     }
 };
+export const getMessage = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;
+        const senderId = req.user._id;
+        const conversation = await Conversation.findOne({
+            members: { $all: [senderId, userToChatId] },
+        }).populate("messages");
+        res.status(200).json(conversation.messages);
+    } catch (error) {
+        console.log("Error in getMessage controller:", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import protechRoute  from '../middlewares/protechRoute.js'
-import { sendMessage } from "../controllers/message.controller.js";
+import { sendMessage,getMessage } from "../controllers/message.controller.js";
 
 
 const router = express.Router();
-
+router.get("/:id",protechRoute,getMessage);
 router.post("/send/:id",protechRoute,sendMessage);
 
 export default router;
