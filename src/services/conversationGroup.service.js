59936487ce31368b493c423e649ace4a7c00fcb0
Luu Trung Nghia <luutrungnghia1901@gmail.com>
2025-04-23 17:32:22 +0700

    gui su kien truoc khi xoa thanh vien ra khoi nhom

--- src/services/conversationGroup.service.js
+++ src/services/conversationGroup.service.js
@@ -129,9 +129,6 @@ export const removeMember = async (requesterId, groupId, userId) => {
     if (requesterId === userId) {
         throw new ForbiddenError("Chủ nhóm không thể tự xóa chính mình khỏi nhóm");
     }
-
-    // Tiến hành xóa user khỏi danh sách thành viên
-    group.participants = group.participants.filter(p => p.user.toString() !== userId);
     group.participants.forEach(p => {
         appSocket.emitToUser(p.user.toString(), 'group:member-removed', {
             groupId,
@@ -139,6 +136,8 @@ export const removeMember = async (requesterId, groupId, userId) => {
             removedBy: requesterId
         });
     });
+    // Tiến hành xóa user khỏi danh sách thành viên
+    group.participants = group.participants.filter(p => p.user.toString() !== userId);
     return await group.save();
 };
 
