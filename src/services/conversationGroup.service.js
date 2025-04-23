203665ede915c57d91ab0fb7b8ff36f5184299a9
lehau17 <haule@hitek.com.vn>
2025-04-23 21:37:12 +0700

    FIx respóne trả về từ get list

--- src/services/conversationGroup.service.js
+++ src/services/conversationGroup.service.js
@@ -371,7 +371,7 @@ export const getFriendsNotInGroup = async (groupId, currentUserId) => {
     // 5. Trả thông tin chi tiết
     const availableFriends = await User.find({
         _id: { $in: availableFriendIds }
-    }).select("_id username avatar");
+    })
 
     return availableFriends;
 };
